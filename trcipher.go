50817255cf62fd0286143fb13f7f86637ead7818
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-02-13 23:51:55 -0600

    works but in need of refactor
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- trcipher.go
+++ trcipher.go
@@ -18,15 +18,11 @@ func main() {
 		grid[i] = make([]rune, size)
 	}
 
-	// populate with junk data
-	for ri := 0; ri < size; ri++ {
-		for ci := 0; ci < size; ci++ {
-			grid[ri][ci] = 'x'
-		}
-	}
+	populate(msg, grid, size)
 
 	fmt.Println(dumpGrid(grid))
 
+	fmt.Println(transposition(grid, size))
 }
 
 func dumpGrid(grid [][]rune) string {
@@ -37,6 +33,50 @@ func dumpGrid(grid [][]rune) string {
 		}
 	}
 	return outstr
+}
+
+func populate(msg string, grid [][]rune, size int) {
+	for row := 0; row < size; row++ {
+		// get the range of the substring
+		start := row * size
+		end := (row + 1) * size
+
+		// dont allow read out of bounds
+		var strblock string
+		if start >= len(msg) {
+			break
+		} else if end >= len(msg) {
+			strblock = string(msg[start:])
+		} else {
+			strblock = string(msg[start:end])
+		}
+
+		// add padding so there is no index out of bounds
+		if len(strblock) < size {
+			diff := size - len(strblock)
+			for i := 0; i < diff; i++ {
+				strblock += " "
+			}
+		}
+
+		for col := 0; col < size; col++ {
+			grid[row][col] = rune(strblock[col])
+
+		}
+	}
+}
+
+func transposition(grid [][]rune, size int) string {
+	var ciphertext string
+	for row := 0; row < size; row++ {
+		for col := 0; col < size; col++ {
+			// invert the read
+			if grid[col][row] != 0 {
+				ciphertext += string(grid[col][row])
+			}
+		}
+	}
+	return ciphertext
 }
 
 // Not going to bring in math for pow
