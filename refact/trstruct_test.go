ed528783f3da5c40e8db2ef635781b56c4887111
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-05-05 23:30:14 -0500

    added test for strip function
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- refact/trstruct_test.go
+++ refact/trstruct_test.go
@@ -11,6 +11,17 @@ func TestGrid(t *testing.T) {
 	})
 }
 
+func TestFunc(t *testing.T) {
+	t.Run("remove all the spaces from the output using strip", func(t *testing.T){
+		got := strip("this is a test")
+		want := "thisisatest"
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
+
 func (g *Grid) dumpGrid() string {
 	var outstr string
 	for _, r := range g.grid {
